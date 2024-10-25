53a7080791069009df64932c332ae6fc6baa6273
nuhy <82856092+NUHYs@users.noreply.github.com>
2024-10-25 14:17:40 +0900

    캐릭수정차 변경

--- Player.py
+++ Player.py
@@ -1,7 +1,7 @@
 import pygame
 import math
 import os
-
+import real_game
 script_dir = os.path.dirname(__file__)
 
 class Player:
@@ -9,3 +9,18 @@ class Player:
         self.pos = pos
         self.x ,self.y, self.z = pos
         self.image = f"{script_dir}//images//{image}"
+        
+class PLAYBlock:
+    def __init__(self, pos, size):
+        self.pos = pos
+        self.x = pos[0]
+        self.y = pos[1]
+        self.z = pos[2]
+        self.size = size
+        self.points = [
+            [self.x - self.size, 2*(self.y - self.size), self.z - self.size, self.z - self.size], [self.x + self.size, 2 *(self.y - self.size), self.z - self.size, self.z - self.size],
+            [self.x + self.size, 2*(self.y + self.size), self.z - self.size, self.z - self.size], [self.x - self.size, 2 *(self.y + self.size), self.z - self.size, self.z - self.size],
+            [self.x - self.size, 2*(self.y - self.size), self.z + self.size, self.z + self.size], [self.x + self.size, 2 *(self.y - self.size), self.z + self.size, self.z + self.size],
+            [self.x + self.size, 2*(self.y + self.size), self.z + self.size, self.z + self.size], [self.x - self.size, 2 *(self.y + self.size), self.z + self.size, self.z + self.size]
+        ]
+playerblock = PLAYBlock((0,175,-500),50)
