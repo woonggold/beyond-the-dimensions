cc5cfe1a630f3c0f4d6a6b7afe3a9ad6905de9b6
nuhy <hyunnow28@gmail.com>
2024-11-06 19:25:53 +0900

    k수정

--- real_game.py
+++ real_game.py
@@ -394,7 +394,7 @@ def event_check():
                     color = 0
             if event.key == pygame.K_k:
                 if z_key_count == 1:
-                    print(player.pos)
+                    print(player.x , player.y, player.z)
             if event.key == pygame.K_p: # 맵 세이브
                 if z_key_count == 1:
                     map_loading.map_save()
